3894fd259fac828a21e5b2503c34aa4e47b137b0
Alexandre Prevost <alexandre.prevost@bidgroup.ca>
2025-11-11 21:48:35 +1100

    Fix initialisation error

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import sys
 import random
 import platform
+
+from os import path
 from pathlib import Path
-from typing import List
 from loguru import logger
 
 from sc2 import maps
@@ -14,9 +15,8 @@ sys.path.append("ares-sc2/src/ares")
 sys.path.append("ares-sc2/src")
 sys.path.append("ares-sc2")
 
-from bot.main import BruceBot
 from ladder import run_ladder_game
-
+from bot.main import BruceBot
 
 plt = platform.system()
 
@@ -25,31 +25,43 @@ if plt == "Windows":
 elif plt == "Darwin":
     MAPS_PATH: str = "/Applications/StarCraft II/Maps"
 elif plt == "Linux":
-    MAPS_PATH: str = ("~/<username>/Games/battlenet/drive_c/Program Files (x86)/StarCraft II/Maps")
+    MAPS_PATH: str = "~/<username>/Games/battlenet/drive_c/Program Files (x86)/StarCraft II/Maps"
 else:
     logger.error(f"{plt} not supported")
     sys.exit()
 
-def main():
-    bot_name: str = "BruceBot"
-    race: Race = Race.Terran
-    player1 = Bot(race, BruceBot(), bot_name)
+MAP_FILE_EXT: str = "SC2Map"
 
-    # Ladder game started by LadderManager
+def main():
+    __user_config_location__: str = path.abspath(".")
+
+    bot_race: Race = Race.Terran
+    bot = Bot(bot_race, BruceBot(), "Bruce")
+
     if "--LadderServer" in sys.argv:
         print("Starting ladder game...")
-        result, opponentid = run_ladder_game(player1)
+        result, opponentid = run_ladder_game(bot)
         print(result, " against opponent ", opponentid)
 
-    # Local game
     else:
-        map_list: List[str] = [p.name.replace(f".SC2Map", "") for p in Path(MAPS_PATH).glob(f"*.SC2Map") if p.is_file()]
-        random_race = random.choice([Race.Zerg, Race.Terran, Race.Protoss])
-        random_difficulty = random.choice([Difficulty.CheatVision])
-        player2 = Computer(random_race, random_difficulty, ai_build=AIBuild.Macro)
-
         print("Starting local game...")
-        run_game(maps.get(random.choice(map_list)),[player1, player2], realtime=False)
+        game_map = maps.get(random.choice([
+            p.name.replace(f".{MAP_FILE_EXT}", "")
+            for p in Path(MAPS_PATH).glob(f"*.{MAP_FILE_EXT}")
+            if p.is_file() # and "Pylon" in p.name
+        ]))
+
+        random_race = random.choice([Race.Zerg, Race.Terran, Race.Protoss])
+        # random_race = Race.Protoss
+        random_difficulty = random.choice([Difficulty.VeryHard, Difficulty.CheatVision, Difficulty.CheatMoney, Difficulty.CheatInsane])
+        # random_difficulty = Difficulty.CheatInsane
+        random_build = AIBuild.RandomBuild
+        # random_build = AIBuild.Macro  # AIBuild.Rush
+
+        player1 = Bot(bot_race, BruceBot(), "Bruce")
+        player2 = Computer(random_race, difficulty=random_difficulty, ai_build=random_build)
+
+        run_game(game_map, [player1, player2], realtime=False)
 
 if __name__ == "__main__":
     main()
