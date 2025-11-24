6cb7020eae576b93344a37db8d7cd1e40e93ce5d
Alexandre Prevost <alexandre.prevost@bidgroup.ca>
2025-11-24 11:26:49 +1100

    BruceBot 2.2.5 Fix pack

--- bot/main.py
+++ bot/main.py
@@ -36,7 +36,7 @@ from bot.utils import add_placements, remove_illegal_positions
 
 class BruceBot(AresBot):
     NAME: str = "BruceBot"
-    VERSION: str = "2.2.4"
+    VERSION: str = "2.2.5"
     CODE_NAME: str = "Fearless"
 
     def __init__(self, game_step_override: Optional[int] = None):
